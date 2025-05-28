a0c0907adb1f2c71f798c0c06739e85183ccb1a2
Yoga Novianto <yoga@intimap.com>
2025-05-28 10:48:45 +0700

    fix validator error, remove extra space validation tag

--- model/auth.go
+++ model/auth.go
@@ -9,5 +9,5 @@ type RegisterUser struct {
 	Username string `json:"username" db:"username" validate:"required"`
 	Password string `json:"password" db:"password" validate:"required"`
 	FullName string `json:"full_name" db:"full_name" validate:"required"`
-	Email    string `json:"email" db:"email" validate:"email, required"`
+	Email    string `json:"email" db:"email" validate:"required,email"`
 }
